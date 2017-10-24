2a4bf2ce87adace08584b47a64acf21a445133c4
Deepak Yadav <deepak.yadav@iitkgp.ac.in>
2017-10-24 22:41:36 +0530

    Complete program added

---
+++ myl.h
@@ -0,0 +1,25 @@
+#ifndef MYL_H
+#define MYL_H
+
+#define OK 0
+#define ERR 1
+
+// prints string of characters
+int prints(char *);
+
+// prints an integer
+int printi(int );
+
+// prints a floating point number
+int printd(float);
+
+// reads an integer
+int readi(int *);
+
+// reads a floating point number
+int readf(float *);
+
+// a utility function to read a single character
+void readChar(char *);
+
+#endif
